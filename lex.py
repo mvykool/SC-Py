300bce42d8195bcf2f66d5163dc02791569b3a91
mvykool <maykolandres4@gmail.com>
2024-07-21 06:07:59 -0500

    created token type class

--- lex.py
+++ lex.py
@@ -1,3 +1,6 @@
+import enum
+
+
 class Lexer:
     def __init__(self, source):
         self.source = source + '\n'
@@ -72,3 +75,37 @@ class Token:
         #  text used for identifiers, strings and numbers
         self.kind == tokenKind
         #  type the token is classified as
+
+#  TokenType is our enum for all our types of tokens
+
+
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    # KEYWORDS
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+    #  OPERATORS
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 110
+    GTEQ = 111
