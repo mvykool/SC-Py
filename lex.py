50c38f40124084a0a45cb4d1f5da1f4a009e545d
mvykool <maykolandres4@gmail.com>
2024-07-21 08:11:31 -0500

    added gitignore

--- lex.py
+++ lex.py
@@ -96,6 +96,20 @@ class Lexer:
                 token = Token(lastChar + self.curChar, TokenType.NOTEQ)
             else:
                 self.abort("Expected !=, got !" + self.peek())
+        elif self.curChar == '/"':
+            #  get characters between quotations
+            self.nextChar()
+            startPos = self.curPos
+
+            while self.curChar != '/"':
+                #  no allow special characters in strings
+                #  no scape characters, newlines, tabs
+                #  we'll be using C printf on this string
+                if self.curChar == '\n' or self.curChar == '\r' or self.curChar == '\t' or self.curChar == '\\' or self.curChar == '%':
+                    self.abort("Illegal characters in string")
+
+                tokText = self.source[startPos: self.curPos]  # get substring
+                token = Token(tokText, TokenType.STRING)
 
         elif self.curChar == '\n':
             token = Token(self.curChar, TokenType.NEWLINE)
