8df27107d9147f8116140ca3eac49fc09c94c2ac
mvykool <maykolandres4@gmail.com>
2024-07-23 04:43:52 -0500

    added initial parser structure

---
+++ parse.py
@@ -0,0 +1,30 @@
+import sys
+from lex import *
+
+# Parser object keeps track of current token
+# and checks if the code matches the grammar
+
+
+class Parser:
+    def __init__(self, lexer):
+        pass
+
+    #  return token if current token matches
+    def checkToken(self, kind):
+        pass
+
+    #  return token if next token matches
+    def checkPeek(self, kind):
+        pass
+
+    # Try to match current token. If not, error.
+    # Advances the current token.
+    def match(self, kind):
+        pass
+
+    # advance current token
+    def nextToken(self):
+        pass
+
+    def abort(self, message):
+        sys.exit("error" + message)
