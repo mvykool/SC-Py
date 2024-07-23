70e2b7fa3ae7f911acb4041a98eabbb2061b4cc6
mvykool <maykolandres4@gmail.com>
2024-07-23 04:37:32 -0500

    updated main

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from lex import *
+from parse import *
+import sys
 
 
 def main():
-    source = "IF+-123 foo*THEN/"
-    lexer = Lexer(source)
+    print("tenny compoiler")
 
-    token = lexer.getToken()
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lexer.getToken()
+    if len(sys.argv) != 2:
+        sys.exit("error compiler needs source file as argument")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
+
+    #  initialize parser and Lexer
+    lexer = Lexer(source)
+    parser = Parser(lexer)
+
+    parser.program()  # start parser
+    print("print parser completed")
 
 
 main()
