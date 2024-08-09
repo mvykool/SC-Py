9a48b4e0ef14d04a66e6e1eae9b211c159387dd0
mvykool <maykolandres4@gmail.com>
2024-08-09 12:00:00 -0500

    fixed input code for C

--- out.c
+++ out.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main (void){
+float nums;
 float a;
 float b;
 float c;
 printf("how many fibonacci numbers do you want?\n");
+if(0 == scanf("%f", &nums)) {
+nums = 0;
+scanf("%*s");
+}
 printf("\n");
 a = ;
 b = ;
