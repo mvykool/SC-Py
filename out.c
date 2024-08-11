d80b3df0eead9f69181ac748f10b1efaccde48bf
mvykool <maykolandres4@gmail.com>
2024-08-11 17:56:36 -0500

    output new code

--- out.c
+++ out.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 int main (void){
-float nums;
 float a;
 float b;
+float s;
 float c;
-printf("how many fibonacci numbers do you want?\n");
-if(0 == scanf("%f", &nums)) {
-nums = 0;
+a = 0;
+while(a<1){
+printf("enter number of scores: \n");
+if(0 == scanf("%f", &a)) {
+a = 0;
 scanf("%*s");
 }
-printf("\n");
-a = ;
-b = ;
-while(){
-printf("%.2f\n", (float) ());
-c = ;
-a = ;
-b = ;
-nums = ;
 }
+b = 0;
+s = 0;
+printf("enter one value at a time: \n");
+while(b<a){
+if(0 == scanf("%f", &c)) {
+c = 0;
+scanf("%*s");
+}
+s = s+c;
+b = b+1;
+}
+printf("average: \n");
+printf("%.2f\n", (float) (s/a));
 return 0;
 }
